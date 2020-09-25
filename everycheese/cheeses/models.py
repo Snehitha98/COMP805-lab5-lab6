6b3963552f2e9ea3dcd7fb3b6ef1cf308df03aa9
vamshi vuupula <vv1020@wildcats.unh.edu>
2020-09-25 17:01:47 -0400

    adding tests in cheeses app

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -6,11 +6,12 @@ class Cheese(TimeStampedModel):
     slug = AutoSlugField("Cheese Address",unique=True, always_update=False, populate_from="name")
     description = models.TextField("Description", blank=True)
 
-
     class Firmness(models.TextChoices):
         UNSPECIFIED = "unspecified", "Unspecified"
         SOFT = "soft", "Soft"
         SEMI_SOFT = "semi-soft", "Semi-Soft"
         SEMI_HARD = "semi-hard", "Semi-Hard"
         HARD = "hard", "Hard"
-        firmness = models.CharField("Firmness", max_length=20,choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+    firmness = models.CharField("Firmness", max_length=20,choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+    def __str__(self):
+        return self.name
