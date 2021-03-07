0063575aa1d83f67e9da8987b6912cf8985f4ad0
snehitha <sm1552@wildcats.unh.edu>
2021-03-07 13:35:20 -0500

    deploying to heroku

--- config/wsgi.py
+++ config/wsgi.py
@@ -28,7 +28,7 @@ sys.path.append(str(ROOT_DIR / "everycheese"))
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
 os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "config.settings.local"
+    "DJANGO_SETTINGS_MODULE", "config.settings.production"
 )
 
 # This application object is used by any WSGI server configured to use this
